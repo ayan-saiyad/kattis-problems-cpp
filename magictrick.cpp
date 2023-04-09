6b88d26da190f8c7d2d09b43aba3ededd87263d7
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-04-09 01:09:50 -0500

    Magic Trick

---
+++ magictrick.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool find(string line, string c) {
+
+    return 
+    std::string::find(c);
+}
+
+int main() {
+    string line;
+    cin >> line;
+
+    for(int i = 0; i < line.length(); i++) {
+
+    }
+
+}

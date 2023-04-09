1e3c1a6a5e48dbb929b2993d5a3ba426d2c9fee3
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-04-09 01:29:04 -0500

    Echo Echo Echo

--- magictrick.cpp
+++ magictrick.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-bool find(string line, string c) {
-
-    return 
-    std::string::find(c);
+bool find(string s, string c) {
+    int i = s.find(c);
+    return i != string::npos;
 }
 
 int main() {
@@ -12,7 +11,11 @@ int main() {
     cin >> line;
 
     for(int i = 0; i < line.length(); i++) {
-
+        string ch = line.substr(i, 1);
+        if (find(line.substr(i+1), ch)) {
+            cout << 0;
+            return 0;
+        }
     }
-
+    cout << 1;
 }

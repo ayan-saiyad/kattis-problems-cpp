fd9779974bc6316e7fbe7447937f6d223f65e0d5
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-04-12 17:10:31 -0500

    Cold-puter Science

---
+++ coldputerscience.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        
+        if (x < 0) count++;
+    }
+    cout << count;
+}

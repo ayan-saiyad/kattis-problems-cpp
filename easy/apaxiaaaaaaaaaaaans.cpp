6956db3537ee905d9ca498780b35a257ed1a8dbc
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-04-17 18:21:17 -0500

    Apaxiaaaaaaaaaaaans!

---
+++ easy/apaxiaaaaaaaaaaaans.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    
+    string ans = "";
+    ans += line[0];
+    for(int i = 1; i < line.size(); i++) {
+        if(line[i] != line[i-1]) {
+            ans += line[i];
+        }
+    }
+    cout << ans;
+}
